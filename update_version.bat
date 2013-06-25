827bda77612bb0e6b1d1f21149e603819ba64db7
renee <jfwu@thoughtworks.com>
2013-06-25 14:43:47 +0800

    change update_version.bat

--- update_version.bat
+++ update_version.bat
@@ -3,7 +3,7 @@ git rev-list --remotes HEAD|wc -l|sed -e 's/ *//g'|xargs -n1 printf %%04d>gitcom
 set /p count=<gitcommitnumber
 REM echo %count%
 
-git show --abbrev-commit HEAD | grep '^commit' | sed -e 's/commit //'>gitcommitcount
+git show --remotes --abbrev-commit HEAD | grep '^commit' | sed -e 's/commit //'>gitcommitcount
 set /p commit=<gitcommitcount
 
 set buildno=%count%.%commit%
